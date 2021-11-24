d46ee6025000bed7d486f055842a03910093cfec
rezakurniawan1 <91168431+rezakurniawan1@users.noreply.github.com>
2021-11-24 10:14:04 +0700

    Add files via upload

---
+++ Prak8_Reza_ELKOM1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 09:51:04 2021
+
+@author: Reza Kurniawan
+"""
+print("--ELKOM 1--")
+a = [2,7,9,6,8]
+print("List: ", a)
+print("Hasil reverse: ", tuple(a))
---
+++ Prak8_Reza_ELKOM3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 10:04:21 2021
+
+@author: Reza Kurniawan
+"""
+print("----ELKOM 3----")
+a = [2,6,7,10,-2,4,5]
+b = 1
+for x in a:
+    b *= x
+print("Hasil kali value  list: ",)
+print(b)
